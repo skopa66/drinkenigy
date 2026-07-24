0520bd4a5c37f320d2dd5c73ad4921c16d8ef548
skopa66 <eniola.osinloye@pau.edu.ng>
2026-07-25 00:02:16 +0100

    Add initial CSS styles for the webpage

---
+++ script.js
@@ -0,0 +1,69 @@
+body{
+margin:0;
+font-family:Arial,sans-serif;
+background:#050505;
+color:white;
+}
+
+nav{
+display:flex;
+justify-content:space-between;
+padding:20px 60px;
+background:#111;
+align-items:center;
+}
+
+nav a{
+color:#00ff99;
+text-decoration:none;
+font-weight:bold;
+}
+
+.hero{
+text-align:center;
+padding:120px 20px;
+background:linear-gradient(135deg,#00ff99,#6a00ff);
+}
+
+.hero h2{
+font-size:60px;
+margin-bottom:10px;
+}
+
+.hero p{
+font-size:22px;
+}
+
+button{
+padding:15px 35px;
+border:none;
+border-radius:30px;
+background:#00ff99;
+font-size:18px;
+font-weight:bold;
+cursor:pointer;
+margin-top:20px;
+}
+
+section{
+padding:60px 10%;
+}
+
+.card{
+background:#111;
+padding:25px;
+margin:20px 0;
+border-radius:20px;
+transition:.3s;
+}
+
+.card:hover{
+transform:translateY(-8px);
+background:#1c1c1c;
+}
+
+footer{
+text-align:center;
+padding:30px;
+background:#111;
+}
